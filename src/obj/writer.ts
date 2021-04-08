ac684fb4057a14f0d93d10339ab945b2adec4c2c
Geir Birkedal <geir@imerso.com>
2021-04-08 14:46:07 +0200

    fix: Combine nodes with the same ID
    In some cases there are more nodes per IFC object.
    Make sure to write them as one object.

--- src/obj/writer.ts
+++ src/obj/writer.ts
@@ -9,6 +9,16 @@ export interface IWriterOptions {
     skipNormals?: boolean;
 }
 
+type NodeID = IMF.NodeID
+type ObjectNodes = Map<string, NodeID[]>
+
+const myFixed4 = (x: number) => {
+    return Math.round(x * 10000)/10000
+}
+const myFixed2 = (x: number) => {
+    return Math.round(x * 100)/100
+}
+
 /**
  * Utility class for serializing parsed 3D content to local file system as OBJ.
  */
@@ -62,14 +72,123 @@ export class Writer {
         return matrix
     }
 
-    private loadGlobalId(idFile: string) : Map<number, string> {
-        const text = fse.readFileSync(idFile, 'utf-8')
-        const entries = text.split("\n").map(s => s.split(" "))
 
-        const ids = new Map<number, string>()
-        entries.forEach(e => ids.set(+e[0], e[1]))
-        return ids
+
+    private getObjectNodes(imf: IMF.IScene, propdb?: PropDbReader) {
+        let buildingNodes = new Map<string, NodeID[]>()
+        let openingNodes = new Map<string, NodeID[]>()
+
+        for (let i = 0; i < imf.getNodeCount(); i++) {
+            const node = imf.getNode(i)
+
+            if (node.kind !== IMF.NodeKind.Object){
+                this.options.log("Skipping node type " + node.kind.toString())
+                continue
+            }
+         
+            let isOpening = false
+            const props = propdb?.getProperties(node.dbid)
+            if (props && props['Element:IfcExportAs'] && props['Element:IfcExportAs'] === 'IfcOpeningElement') {
+                isOpening = true
+            }
+
+            const id = propdb?.findPropertyRecursive(node.dbid, ['IFC:GLOBALID', 'Element:IfcGUID']) ??`dbid-${node.dbid}`
+            
+            if (isOpening) {
+                openingNodes.get(id)?.push(i) ?? openingNodes.set(id, [i])
+            } else {
+                buildingNodes.get(id)?.push(i) ?? buildingNodes.set(id, [i])
+            }
+        }
+
+        return {buildingNodes: buildingNodes, openingNodes: openingNodes}
     }
+
+
+    private writeObjects(objectNodes: ObjectNodes, outputPath: string, scene: IMF.IScene, rootScale: number) {
+
+        fse.writeFileSync(outputPath, "");
+
+        let output : string[] = []
+        let indexOffset = 1
+
+        objectNodes.forEach((nodes: NodeID[], id: string) => {
+
+            output.push("o " + id)
+            
+            nodes.forEach(nodeId => {
+                
+                const node = scene.getNode(nodeId)
+
+                if (node.kind !== IMF.NodeKind.Object){
+                    throw Error(`Node kind ${node.kind} should have been excluded`)
+                }
+
+                const geometry = scene.getGeometry(node.geometry)
+
+                if (geometry.kind === IMF.GeometryKind.Mesh){
+        
+                    const transform = this.nodeTransform(node)
+                    const rotation = new THREE.Matrix4().extractRotation(transform)
+        
+                    const indices = geometry.getIndices()
+                    const vertices = geometry.getVertices()                    
+        
+                    const numVertices = vertices.length / 3
+                    const numIndices = indices.length / 3
+        
+                    const vIt = vertices.values()
+                    for (let v = 0; v < numVertices; v++){
+                        const pos = new THREE.Vector3(vIt.next().value, vIt.next().value, vIt.next().value)
+                        pos.applyMatrix4(transform).multiplyScalar(rootScale)
+                        output.push(`v ${myFixed4(pos.x)} ${myFixed4(pos.y)} ${myFixed4(pos.z)}`)
+                    }
+        
+                    let writingNormals = false
+                    if (!this.options.skipNormals){
+                        const normals = geometry.getNormals()
+                        if (normals){
+                            writingNormals = true
+                            const nIt = normals.values()
+                            for (let i = 0; i < normals.length / 3; i++){
+                                const n = new THREE.Vector3(nIt.next().value, nIt.next().value, nIt.next().value)
+                                n.applyMatrix4(rotation)
+                                output.push(`vn ${myFixed2(n.x)} ${myFixed2(n.y)} ${myFixed2(n.z)}`)
+                            }
+                        }
+                    }
+        
+                    const faceString = (f0: number, f1: number, f2: number, withNormals: boolean) => {
+                        if (withNormals){
+                            return `f ${f0}//${f0} ${f1}//${f1} ${f2}//${f2}`
+                        } else {
+                            return `f ${f0} ${f1} ${f2}`
+                        }
+                    }
+        
+                    const fIt = indices.values()
+                    for (let f = 0; f < numIndices; f++){
+        
+                        const f0 = indexOffset + fIt.next().value
+                        const f1 = indexOffset + fIt.next().value
+                        const f2 = indexOffset + fIt.next().value
+        
+                        output.push(faceString(f0, f1, f2, writingNormals))
+                    }
+        
+                    indexOffset += numVertices
+        
+                    if (output.length > 50000){
+                        fse.appendFileSync(outputPath, output.join("\n") + "\n");
+                        output = []
+                    }
+                }
+            })
+        })
+        
+        fse.appendFileSync(outputPath, output.join("\n"));
+    }
+
 
     /**
      * Outputs scene into OBJ.
@@ -84,114 +203,11 @@ export class Writer {
         const openingObjPath = path.join(outputDir, 'openings.obj');
 
         const rootScale = this.getRootScale(imf)
-        let regularIndexOffset = 1
-        let openingIndexOffset = 1
-        let regularOutput : string[] = []
-        let openingOutput : string[] = []
         
-        fse.writeFileSync(objPath, "");
-        fse.writeFileSync(openingObjPath, "");
+        const nodeInfo = this.getObjectNodes(imf, propdb)
 
-        const myFixed4 = (x: number) => {
-            return Math.round(x * 10000)/10000
-        }
-        const myFixed2 = (x: number) => {
-            return Math.round(x * 100)/100
-        }
-
-        for (let i = 0; i < imf.getNodeCount(); i++) {
-            const node = imf.getNode(i)
-
-            if (node.kind !== IMF.NodeKind.Object){
-                this.options.log("Skipping node type " + node.kind.toString())
-                continue
-            }
-         
-            const geometry = imf.getGeometry(node.geometry)
-
-            if (geometry.kind !== IMF.GeometryKind.Mesh){
-                this.options.log("Skipping geometry type " + geometry.kind.toString())
-                continue
-            }
-
-            let isOpening = false
-            const props = propdb?.getProperties(node.dbid)
-            if (props && props['Element:IfcExportAs'] && props['Element:IfcExportAs'] === 'IfcOpeningElement') {
-                isOpening = true
-            }
-
-            const output = isOpening ? openingOutput : regularOutput
-
-            const transform = this.nodeTransform(node)
-            const rotation = new THREE.Matrix4().extractRotation(transform)
-
-            const indices = geometry.getIndices()
-            const vertices = geometry.getVertices()                    
-
-            const id = propdb?.findPropertyRecursive(node.dbid, ['IFC:GLOBALID', 'Element:IfcGUID']) ??`dbid-${node.dbid}`
-            output.push("o " + id)
-                                
-            const numVertices = vertices.length / 3
-            const numIndices = indices.length / 3
-
-            const vIt = vertices.values()
-            for (let v = 0; v < numVertices; v++){
-                const pos = new THREE.Vector3(vIt.next().value, vIt.next().value, vIt.next().value)
-                pos.applyMatrix4(transform).multiplyScalar(rootScale)
-                output.push(`v ${myFixed4(pos.x)} ${myFixed4(pos.y)} ${myFixed4(pos.z)}`)
-            }
-
-            let writingNormals = false
-            if (!this.options.skipNormals){
-                const normals = geometry.getNormals()
-                if (normals){
-                    writingNormals = true
-                    const nIt = normals.values()
-                    for (let i = 0; i < normals.length / 3; i++){
-                        const n = new THREE.Vector3(nIt.next().value, nIt.next().value, nIt.next().value)
-                        n.applyMatrix4(rotation)
-                        output.push(`vn ${myFixed2(n.x)} ${myFixed2(n.y)} ${myFixed2(n.z)}`)
-                    }
-                }
-            }
-
-            const faceString = (f0: number, f1: number, f2: number, withNormals: boolean) => {
-                if (withNormals){
-                    return `f ${f0}//${f0} ${f1}//${f1} ${f2}//${f2}`
-                } else {
-                    return `f ${f0} ${f1} ${f2}`
-                }
-            }
-
-            const indexOffset = isOpening ? openingIndexOffset : regularIndexOffset
-
-            const fIt = indices.values()
-            for (let f = 0; f < numIndices; f++){
-
-                const f0 = indexOffset + fIt.next().value
-                const f1 = indexOffset + fIt.next().value
-                const f2 = indexOffset + fIt.next().value
-
-                output.push(faceString(f0, f1, f2, writingNormals))
-            }
-
-            if (isOpening)
-                openingIndexOffset += numVertices
-            else
-                regularIndexOffset += numVertices
-
-            if (regularOutput.length > 50000){
-                fse.appendFileSync(objPath, regularOutput.join("\n") + "\n");
-                regularOutput = []
-            }
-            if (openingOutput.length > 50000){
-                fse.appendFileSync(openingObjPath, openingOutput.join("\n") + "\n");
-                openingOutput = []
-            }
-        }
-
-        fse.appendFileSync(objPath, regularOutput.join("\n"));
-        fse.appendFileSync(openingObjPath, openingOutput.join("\n"));
+        this.writeObjects(nodeInfo.buildingNodes, objPath, imf, rootScale)
+        this.writeObjects(nodeInfo.openingNodes, openingObjPath, imf, rootScale)
 
         this.options.log(`Finished writing OBJ`);
     }
